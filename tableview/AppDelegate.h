99518c432ae07f4aed6a16827b3376274964089f
jacknazi <linwei.ios@bytedance.com>
2019-09-08 17:14:33 +0800

    Initial Commit

---
+++ tableview/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tableview
+//
+//  Created by linwei on 2019/9/8.
+//  Copyright © 2019 bytedance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tableview/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tableview
+//
+//  Created by linwei on 2019/9/8.
+//  Copyright © 2019 bytedance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
